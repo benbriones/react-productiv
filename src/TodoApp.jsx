97bc3b66d4b3ecf664fdc9745f6c38661988f521
Ben Briones <benbriones50@gmail.com>
2024-04-04 14:54:55 -0700

    added create, update, remove, started editable todos

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -15,18 +15,26 @@ import EditableTodoList from "./EditableTodoList";
  * App -> TodoApp -> { TodoForm, EditableTodoList }
  */
 
-function TodoApp() {
+function TodoApp({ initialTodos }) {
+
+  const [todos, setTodos] = useState(initialTodos);
 
   /** add a new todo to list */
   function create(newTodo) {
+    const addTodo = {...newTodo, id: uuid()}
+    setTodos((curr) => [...curr, addTodo]);
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
+    setTodos((currTodos) =>
+      currTodos.map((todo) => (todo.id === updatedTodo.id ? updatedTodo : todo))
+    )
   }
 
   /** delete a todo by id */
   function remove(id) {
+    setTodos((currTodos) => currTodos.filter((todo) => todo.id !== id));
   }
 
   return (
@@ -34,15 +42,16 @@ function TodoApp() {
         <div className="row">
 
           <div className="col-md-6">
-            <EditableTodoList /> OR
+            {todos.length > 0 ?
+            <EditableTodoList todos={todos} update={update} remove={remove}/> :
             <span className="text-muted">You have no todos.</span>
+            }
           </div>
 
           <div className="col-md-6">
-            (if no top todo, omit this whole section)
             <section className="mb-4">
               <h3>Top Todo</h3>
-              <TopTodo />
+              {todos.length > 0 ? <TopTodo /> : <div>No todos yet</div>}
             </section>
 
             <section>
